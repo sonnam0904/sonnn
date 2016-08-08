9e0b46eb710fa8dded9120ab5f6f74aae3668028
sonnguyennam <sonnguyennam@vccorp.vn>
2016-08-08 16:46:18 +0700

    up

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	_ "sonnn/docs"
+	_ "sonnn/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
